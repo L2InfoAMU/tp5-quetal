407ff59207838748422dd19e858d072a53b4d107
o16008843 <MrStalkeur06@gmail.com>
2018-12-05 10:29:44 +0100

    Test

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -11,6 +11,7 @@ public class BruteRasterImage implements Image {
     public  BruteRasterImage (Color color,int width,int height){
         this.width=width;
         this.height=height;
+        System.out.println("Test");
         for (int matriceWidth=0;matriceWidth<width;matriceWidth++)
             for(int matriceHeight=0;matriceHeight<height;matriceHeight++)
                 this.colors[matriceHeight][matriceHeight]=color;
