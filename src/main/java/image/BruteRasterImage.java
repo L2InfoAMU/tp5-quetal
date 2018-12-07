f231377d3cf15b9033ef2d5f1ed86b122be9d7a7
Shulyvan <mrstalkeur06@gmail.com>
2018-12-07 17:50:37 +0100

    Implementation des méthodes createrepresentation, setpixelcolor, setwidth ,setheight,getwidth,setheight

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -8,33 +8,53 @@ public class BruteRasterImage implements Image {
     private int width;
     private int height;
 
-    public  BruteRasterImage (Color color,int width,int height){
-        this.width=width;
-        this.height=height;
+    public BruteRasterImage(Color color, int width, int height) {
+        this.width = width;
+        this.height = height;
         colors = new Color[width][height];
-        for(int widthImage=0; widthImage < width ; widthImage++) {
-            for (int heightImage = 0; heightImage < height; heightImage++){
-                this.colors[heightImage][widthImage] = color;}
-        }}
-        public BruteRasterImage(Color[][] colors){
-            Matrices.requiresNonNull(colors);
-            Matrices.requiresNonZeroDimensions(colors);
-            this.colors=colors;
-
+        for (int widthImage = 0; widthImage < width; widthImage++) {
+            for (int heightImage = 0; heightImage < height; heightImage++) {
+                this.colors[heightImage][widthImage] = color;
+            }
         }
+    }
+
+    public BruteRasterImage(Color[][] colors) {
+        Matrices.requiresNonNull(colors);
+        Matrices.requiresNonZeroDimensions(colors);
+        this.colors = colors;
+
+    }
 
 
     @Override
     public Color getPixelColor(int x, int y) {
-        return null;
+        return colors[x][y];
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return colors.length;
     }
 
     @Override
     public int getWidth() {
-        return 0;
-    }}
+        return colors[0].length;
+    }
+
+    public void createRepresentation() {
+        this.colors = colors;
+    }
+
+    public void setPixelsColor(Color[][] pixels) {
+
+    }
+    protected void setWidth(int width){
+        this.width=width;
+
+    }
+    protected void setHeight(int height){
+        this.height=height;
+
+    }
+}
